0a518d1f6e4fac674f7a78957dd71bb279e5b6d7
duncan020313 <dongjae.lee@prosys.kaist.ac.kr>
2026-03-18 19:10:31 -0700

    [Misc] convert expecto and nl-2-postcond to submodules

--- expecto/src/tasks/__init__.py
+++
@@ -1,9 +0,0 @@
-from .apps import apps
-from .defects4j import defects4j
-from .humaneval_plus import humaneval_plus
-
-task_map = {
-    "apps": apps,
-    "defects4j": defects4j,
-    "humaneval_plus": humaneval_plus,
-}
--- expecto/src/utils/verify.py
+++
@@ -1,8 +0,0 @@
-from inspect_ai.solver import TaskState
-
-
-def calc_communication_length(state: TaskState) -> int:
-    """
-    Calculate the communication length of the task state
-    """
-    return len(state.messages)
